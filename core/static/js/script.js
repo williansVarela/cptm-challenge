f06c5c12acbcb98213ece50563332a1cec4d13ca
Willians Varela <willians.varela@gmail.com>
2020-11-18 20:19:00 -0300

    update charts on dashboard

--- core/static/js/script.js
+++ core/static/js/script.js
@@ -3,25 +3,27 @@
         var myChart = new Chart(ctx, {
             type: 'bar',
             data: {
-                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                labels: ["Rubi", "Diamante", "Esmeralda", "Turquesa", "Coral", "Safira", "Jade"],
                 datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
+                    label: '# Ocorrências',
+                    data: [12, 19, 3, 5, 2, 3, 1],
                     backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
+                        'rgba(168, 3, 79, 0.2)',
+                        'rgba(163, 158, 140, 0.2)',
+                        'rgba(0, 170, 158, 0.2)',
+                        'rgba(0, 130, 155, 0.2)',
+                        'rgba(245, 95, 26, 0.2)',
+                        'rgba(28, 20, 107, 0.2)',
+                        'rgba(0, 176, 82, 0.2)'
                     ],
                     borderColor: [
-                        'rgba(255,99,132,1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
+                        '#a8034f',
+                        '#a39e8c',
+                        '#00aa9e',
+                        '#00829b',
+                        '#f55f1a',
+                        '#1c146b',
+                        '#00b052'
                     ],
                     borderWidth: 1
                 }]
@@ -34,24 +36,6 @@
                         }
                     }]
                 }
-            }
-        });
-
-        //pie
-        var ctxP = document.getElementById("pieChart").getContext('2d');
-        var myPieChart = new Chart(ctxP, {
-            type: 'pie',
-            data: {
-                labels: ["Red", "Green", "Yellow", "Grey", "Dark Grey"],
-                datasets: [{
-                    data: [300, 50, 100, 40, 120],
-                    backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
-                    hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
-                }]
-            },
-            options: {
-                responsive: true,
-                legend: false
             }
         });
 
@@ -142,24 +126,6 @@
         });
  new WOW().init();
 
-        // Regular map
-        function regular_map() {
-            var var_location = new google.maps.LatLng(40.725118, -73.997699);
-
-            var var_mapoptions = {
-                center: var_location,
-                zoom: 14
-            };
-
-            var var_map = new google.maps.Map(document.getElementById("map-container"),
-                var_mapoptions);
-
-            var var_marker = new google.maps.Marker({
-                position: var_location,
-                map: var_map,
-                title: "New York"
-            });
-        }
 
         // Initialize maps
         google.maps.event.addDomListener(window, 'load', regular_map);
